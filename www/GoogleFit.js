ab17803773bedd99b5a1f26be36bee7dfd7e0543
dwaffe <marcin.budzinski@symetria.pl>
2016-05-09 16:03:32 +0200

    formatowanie wciec

--- www/GoogleFit.js
+++ www/GoogleFit.js
@@ -3,30 +3,34 @@ function GoogleFit() {
 }
 
 GoogleFit.prototype.connect = function (successCallback, failureCallback) {
-  cordova.exec(successCallback,
-               failureCallback,
-               "GoogleFit",
-               "connect");
+    cordova.exec(
+        successCallback,
+        failureCallback,
+        "GoogleFit",
+        "connect"
+    );
 };
 
 GoogleFit.prototype.getActivities = function (startTime, endTime, successCallback, failureCallback) {
-  cordova.exec(successCallback,
-               failureCallback,
-               "GoogleFit",
-               "getActivities",
-               [{
-                 "startTime" : startTime,
-                 "endTime" : endTime,
-               }]);
+    cordova.exec(
+        successCallback,
+        failureCallback,
+        "GoogleFit",
+        "getActivities",
+        [{
+            "startTime" : startTime,
+            "endTime" : endTime
+        }]
+    );
 };
 
 GoogleFit.install = function () {
-  if (!window.plugins) {
-    window.plugins = {};
-  }
+    if (!window.plugins) {
+        window.plugins = {};
+    }
 
-  window.fitatu.plugin.googleFit = new GoogleFit();
-  return window.fitatu.plugin.googleFit;
+    window.fitatu.plugin.googleFit = new GoogleFit();
+    return window.fitatu.plugin.googleFit;
 };
 
 cordova.addConstructor(GoogleFit.install);
